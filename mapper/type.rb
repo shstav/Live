406be3d030c94cc43f2ee7feb00185bbe958fcf7
Stav <stav@ftbpro.com>
2015-02-11 17:33:13 +0200

    Mapper Types

---
+++ mapper/type.rb
@@ -0,0 +1,14 @@
+module Live
+  module Mapper
+    module Type
+
+      MATCH         = "Live::Match"
+      EVENT         = "Live::Event::Base"
+      LEAGUE_SEASON = "LeagueSeason"
+      TEAM          = "Subject"
+      PLAYER        = "Player"
+
+    end
+  end
+end
+
